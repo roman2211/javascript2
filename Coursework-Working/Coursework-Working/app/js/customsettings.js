b2caf5734db1122152ea5727ee572959be4682f8
roman2211 <89628270095@yandex.ru>
2018-12-04 22:17:36 +0700

    Coursework

---
+++ Coursework-Working/Coursework-Working/app/js/customsettings.js
@@ -0,0 +1,149 @@
+$(document).ready(function () {
+
+  $(".select-box-it").selectBoxIt({
+    downArrowIcon: "icon-hand-down"
+  });
+
+
+  $(".slide-two").owlCarousel({
+
+    items: 1,
+    loop: true,
+    center: false,
+    rewind: false,
+
+    mouseDrag: true,
+    touchDrag: true,
+    pullDrag: true,
+    freeDrag: false,
+
+    margin: 0,
+    stagePadding: 0,
+
+    autoplay: true,
+    autoplayTimeout: 5000,
+    autoplayHoverPause: false,
+    autoplaySpeed: false,
+
+    merge: false,
+    mergeFit: true,
+    autoWidth: false,
+
+    startPosition: 0,
+    rtl: false,
+
+    smartSpeed: 250,
+    fluidSpeed: false,
+    dragEndSpeed: false,
+
+    responsive: {},
+    responsiveRefreshRate: 200,
+    responsiveBaseElement: window,
+
+    fallbackEasing: 'swing',
+
+    info: false,
+
+    nestedItemSelector: false,
+    itemElement: 'div',
+    stageElement: 'div',
+
+    refreshClass: 'owl-refresh',
+    loadedClass: 'owl-loaded',
+    loadingClass: 'owl-loading',
+    rtlClass: 'owl-rtl',
+    responsiveClass: 'owl-responsive',
+    dragClass: 'owl-drag',
+    itemClass: 'owl-item',
+    stageClass: 'owl-stage',
+    stageOuterClass: 'owl-stage-outer',
+    grabClass: 'owl-grab'
+
+  });
+  //  конец 1 й карусели
+
+  $(".slide-one").owlCarousel({
+
+    items: 1,
+    loop: true,
+    center: false,
+    rewind: false,
+
+    mouseDrag: true,
+    touchDrag: true,
+    pullDrag: true,
+    freeDrag: false,
+
+    margin: 0,
+    stagePadding: 0,
+
+    autoplay: false,
+    autoplayTimeout: 5000,
+    autoplayHoverPause: false,
+    autoplaySpeed: false,
+
+    merge: false,
+    mergeFit: true,
+    autoWidth: false,
+
+    startPosition: 0,
+    rtl: false,
+
+    smartSpeed: 250,
+    fluidSpeed: false,
+    dragEndSpeed: false,
+
+    responsive: {},
+    responsiveRefreshRate: 200,
+    responsiveBaseElement: window,
+
+    fallbackEasing: 'swing',
+
+    info: false,
+
+    nestedItemSelector: false,
+    itemElement: 'div',
+    stageElement: 'div',
+
+    refreshClass: 'owl-refresh',
+    loadedClass: 'owl-loaded',
+    loadingClass: 'owl-loading',
+    rtlClass: 'owl-rtl',
+    responsiveClass: 'owl-responsive',
+    dragClass: 'owl-drag',
+    itemClass: 'owl-item',
+    stageClass: 'owl-stage',
+    stageOuterClass: 'owl-stage-outer',
+    grabClass: 'owl-grab',
+
+    nav: true,
+    navText: ['', ''],
+    navSpeed: false,
+    navElement: 'div',
+    navContainer: false,
+    navContainerClass: 'owl-nav',
+    navClass: ['owl-prev', 'owl-next'],
+    slideBy: 1,
+    dotClass: 'owl-dot',
+    dotsClass: 'owl-dots',
+    dots: true,
+    dotsEach: false,
+    dotsData: false,
+    dotsSpeed: false,
+    dotsContainer: false
+
+  });
+
+  $("#slider-range").slider({
+    range: true,
+    min: 0,
+    max: 1000,
+    values: [52, 400],
+    slide: function (event, ui) {
+      $("#amount").val("$" + ui.values[0] + " - $" + ui.values[1]);
+    }
+  });
+  $("#amount").val("$" + $("#slider-range").slider("values", 0) +
+    " - $" + $("#slider-range").slider("values", 1));
+
+});
